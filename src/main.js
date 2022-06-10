6efdc2e360ffc470060cffb6604f268f18866db5
phoenixredwolf <b.vogtman@outlook.com>
2022-06-10 17:41:15 -0400

    added aws amplify

--- src/main.js
+++ src/main.js
@@ -1,21 +1,5 @@
 import { createApp } from 'vue'
-import {createRouter, createWebHistory} from "vue-router";
 import App from './App.vue'
-import BootstrapVue from 'bootstrap-vue'
-import "bootstrap/dist/css/bootstrap.min.css"
-import "bootstrap/dist/js/bootstrap.min"
+import router from './router'
 
-const Home = { template: '<div>HomePage</div>' }
-const Login = { template: '<div>LogIn</div>'}
-
-const routes = [
-    { path: '/', component: Home },
-    { path: '/login', component: Login}
-]
-
-const router = createRouter({
-    history: createWebHistory(),
-    routes,
-})
-
-createApp(App).use(BootstrapVue).use(router).mount('#app')
+createApp(App).use(router).mount('#app')
---
+++ src/views/HomeView.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <HomePage/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HomePage from '@/components/HomePage.vue'
+
+export default {
+  name: 'HomeView',
+  components: {
+    HomePage
+  }
+}
+</script>
